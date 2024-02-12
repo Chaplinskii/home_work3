15249d9f29305a96e22470db78cc38c5bba55233
Chaplinskii <6945053@gmail.com>
2024-02-12 22:51:21 +0300

    add home work: task1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1:
+//  Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+
+string CriatStr(char[,] ch)
+{
+    string result = "";
+    for (int i = 0; i < ch.GetLongLength(0); i++)
+    {
+        for (int j = 0; j < ch.GetLength(1); j++)
+        {
+            result += ch[i, j];
+        }
+    }
+    return result;
+}
+Console.Clear();
+char[,] array = { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+Console.WriteLine(CriatStr(array));
