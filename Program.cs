58b52e1036c77fe6eb276693a28bb03a925ae5af
Chaplinskii <6945053@gmail.com>
2024-02-14 17:46:50 +0300

    add home work:task3

--- Program.cs
+++ Program.cs
@@ -24,36 +24,61 @@
 // заглавные буквы заменены на строчные. 
 
 
-int[] convert(string str)
-{
-    int n = str.Length;
-    char chars = ' ';
-    int[] utf8 = new int[n];
-    for (int i = 0; i < str.Length; i++)
-    {
-        chars = Convert.ToChar(str[i]);
-        utf8[i] = Convert.ToInt32(chars);
-    }
-    return utf8;
-}
-string change(int[] array)
-{
-    char chars = ' ';
-    string result = "";
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] >= 65 && array[i] <= 90)
-        {
-            array[i] += 32;
+// int[] convert(string str)
+// {
+//     int n = str.Length;
+//     char chars = ' ';
+//     int[] utf8 = new int[n];
+//     for (int i = 0; i < str.Length; i++)
+//     {
+//         chars = Convert.ToChar(str[i]);
+//         utf8[i] = Convert.ToInt32(chars);
+//     }
+//     return utf8;
+// }
+// string change(int[] array)
+// {
+//     char chars = ' ';
+//     string result = "";
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] >= 65 && array[i] <= 90)
+//         {
+//             array[i] += 32;
+//         }
+//         chars = Convert.ToChar(array[i]);
+//         result += Convert.ToString(chars);
+//     }
+//         return result;
+// }
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string str = Console.ReadLine()!;
+// int[] utf8=convert(str);
+// Console.WriteLine(change(utf8));
+
+// Задача 3: 
+// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+bool Answer (string str){
+    for (int i=0;i<str.Length/2;i++){
+        if (str[i]!=str[str.Length-1-i]){
+            return false;
         }
-        chars = Convert.ToChar(array[i]);
-        result += Convert.ToString(chars);
+        
     }
-        return result;
+    return true;
+}
+void PrintAnswer(bool answer){
+    if (answer==true){
+    Console.WriteLine("Yes");
+    }
+    else {
+    Console.WriteLine("No");
+    }
 }
 Console.Clear();
 Console.Write("Введите строку: ");
 string str = Console.ReadLine()!;
-int[] utf8=convert(str);
-// change(utf8);
-Console.WriteLine(change(utf8));
+PrintAnswer(Answer(str));
